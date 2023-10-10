30efca9d0d7749ea2163f23bdb826e27955952ce
MALLIKARJUN SWAMI <ITSARJUNSWAMI@GMAIL.COM>
2023-10-10 18:01:18 +0530

    second Commit

---
+++ web/modules/custom/hello/src/Form/helloConfigForm.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\hello\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure hello settings for this site.
+ */
+class helloSettingsForm extends ConfigFormBase {
+
+  /** 
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'hello.settings';
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'hello_admin_settings';
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+
+    $form['hello_thing'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Things'),
+      '#default_value' => $config->get('hello_thing'),
+    ];  
+
+    $form['other_things'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Other things'),
+      '#default_value' => $config->get('other_things'),
+    ];  
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration.
+    $this->config(static::SETTINGS)
+      // Set the submitted configuration setting.
+      ->set('hello_thing', $form_state->getValue('hello_thing'))
+      // You can set multiple configurations at once by making
+      // multiple calls to set().
+      ->set('other_things', $form_state->getValue('other_things'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
